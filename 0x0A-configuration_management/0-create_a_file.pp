ce70feea1eca715b8472c7aad830bca205f45385
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-12-13 01:36:09 -0800

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
 #create a file with puppet
-file {'title':
-	path => '/tmp/school'
-	mode => 0744
-	group => 'www-data'
-	owner => 'www-data'
-	content => 'I love Puppet'
+file {'/tmp/school':
+
+  ensure  => 'present',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet'
+
 }
