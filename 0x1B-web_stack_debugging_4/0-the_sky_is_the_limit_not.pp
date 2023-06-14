4b82507396959ff83e7a5bbc38e760f3f462dcb0
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-06-14 07:18:03 -0700

    WDB4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# puppet file to fix web_stack debugging 4
+
+exec { 'fix nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => '/bin:/usr/bin:/usr/sbin'
+}
